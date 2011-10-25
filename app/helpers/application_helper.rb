1a3dd55730ed4d451333a474472f38ad25b03eec
Cihad Paksoy <cihadp@gmail.com>
2011-10-25 12:37:45 +0300

    added layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def body_class(classes)
+    content_for(:body_class) { classes }
+  end
 end
