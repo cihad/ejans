b3a5909d62f63c174c00bdcc254595f81ddf087e
Cihad Paksoy <cihadp@gmail.com>
2011-10-26 14:17:45 +0300

    added style for  devise form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
   def body_class(classes)
     content_for(:body_class) { classes }
   end
+
+  def menu_list(text, path, method)
+    if current_page?(path)
+      li_class = "active"
+    end
+    content_tag :li, link_to(text, path, :method => method ), :class => li_class
+  end
 end
