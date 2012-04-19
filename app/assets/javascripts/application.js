827bce57061f21b77b19388ca64dbf4d850c5990
Cihad Paksoy <cihadp@gmail.com>
2012-04-19 19:04:12 +0300

    tinymce fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -67,8 +67,8 @@ $(document).ready(function() {
   // TinyMCE
   $('.tinymce').tinymce({
     theme : "advanced",
-    plugins : "fullscreen,table,paste,advimage",
-    theme_advanced_buttons1 : "bold,italic,underline,strikethrough,justifyleft,justifycenter,justifyright,bullist,numlist,link,unlink,image,formatselect,fullscreen,code,pastetext",
+    plugins : "lists,fullscreen,table,paste,advimage",
+    theme_advanced_buttons1 : "bold,italic,underline,strikethrough,outdent,indent,bullist,numlist,link,unlink,image,formatselect,fullscreen,code,pastetext",
     theme_advanced_buttons2 : "",
     theme_advanced_buttons3 : "",
     theme_advanced_blockformats : "p,h1,h2,h3,h4,h5,h6,blockquote,dt,dd,code",
@@ -76,7 +76,8 @@ $(document).ready(function() {
     theme_advanced_toolbar_align : "left",
     theme_advanced_statusbar_location : "bottom",
     theme_advanced_resizing : true,
-    entity_encoding : "raw"
+    entity_encoding : "raw",
+    content_css : "/assets/typography.css"
   });
 
   // Pagination
