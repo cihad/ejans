e61a74b082a444bb9fd98cd7177f8ff0e9ee0291
Cihad Paksoy <cihadp@gmail.com>
2012-04-20 11:48:04 +0300

    added some allowed html tags

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,6 +1,6 @@
 module NotificationsHelper
   def format_desc(desc)
-    allowed_tags = %w(p strong em a table tbody thead tr td h1 h2 h3 h4 h5 h6 ol ul li quote)
+    allowed_tags = %w(p br strong em a table tbody thead tr td h1 h2 h3 h4 h5 h6 ol ul li quote img)
     sanitize desc, tags: allowed_tags
   end
 end
