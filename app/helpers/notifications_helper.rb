b523cb7f801afd83307ca092edd9e2ef2329f84c
Cihad Paksoy <cihadp@gmail.com>
2012-04-27 15:46:31 +0300

    conanical helper for notification

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,4 +3,12 @@ module NotificationsHelper
     allowed_tags = %w(p br strong em a table tbody thead tr td h1 h2 h3 h4 h5 h6 ol ul li quote img)
     sanitize desc, tags: allowed_tags
   end
+
+  def conanical(notification = @notification)
+    service = notification.services_notifications.first.service
+    path = service_notification_path(service ,notification) 
+    if notification.services_notifications.count > 1
+      conanical_url path
+    end
+  end
 end
