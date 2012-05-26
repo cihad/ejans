2879fa422cd4c51b0fa78751f881ed6cae48c865
Cihad Paksoy <cihadp@gmail.com>
2012-05-26 17:46:11 +0300

    populate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+1000.times do
+  node_type = NodeType.all.sort_by { rand }.first
+  node = node_type.nodes.build(title: Faker::Lorem.sentence)
+
+  node_type.feature_configurations.each do |fea_conf|
+    if node.features.map(&:feature_configuration_id).include?(fea_conf.id)
+      feature = node.features.where(feature_configuration_id: fea_conf.id.to_s).first
+    else
+      feature = node.features.build
+      feature.feature_configuration = fea_conf
+      feature.send("build_#{fea_conf.feature_type}")
+    end
+    feature.feature_object.class.add_value(fea_conf.value_name)
+    feature.feature_object.send("#{fea_conf.value_name}=", rand(1000))
+  end
+  node.save
+end
+
