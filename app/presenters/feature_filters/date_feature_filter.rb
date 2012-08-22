d7d3b1f941b6d10dfd34cf65a98391769503588b
Cihad Paksoy <cihadp@gmail.com>
2012-08-22 10:33:17 +0300

    html5 min max input values

--- app/presenters/feature_filters/date_feature_filter.rb
+++ app/presenters/feature_filters/date_feature_filter.rb
@@ -7,5 +7,13 @@ module FeatureFilters
     def end_machine_name
       "#{machine_name}_end".to_sym
     end
+
+    def start_year
+      conf.start_year
+    end
+
+    def end_year
+      conf.end_year
+    end
   end
 end
