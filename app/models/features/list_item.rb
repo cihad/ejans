44b63d8cb91718349a1e748e61deccd0ee800d4b
Cihad Paksoy <cihadp@gmail.com>
2012-09-05 15:03:22 +0300

    added mongoid document

--- app/models/features/list_item.rb
+++ app/models/features/list_item.rb
@@ -1,5 +1,6 @@
 module Features
   class ListItem
+    include Mongoid::Document
     belongs_to :list_feature_configuration, class_name: "Features::ListFeatureConfiguration"
     field :name, type: String
     validates :name, presence: :true
