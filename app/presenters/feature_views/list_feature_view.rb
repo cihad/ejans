8dfe8bdb1ff498c04b62d1fc4f344891b89bcc2d
Cihad <cihadp@gmail.com>
2012-09-05 04:48:45 +0300

    deleted feature view presenters

--- app/presenters/feature_views/list_feature_view.rb
+++
@@ -1,9 +0,0 @@
-module FeatureViews
-  class ListFeatureView < FeatureView
-    def value
-      feature_tag do
-        feature.value.map(&:name).join(', ')
-      end
-    end
-  end
-end
--- app/presenters/feature_views/string_feature_view.rb
+++
@@ -1,9 +0,0 @@
-module FeatureViews
-  class StringFeatureView < FeatureView
-    def value
-      feature_tag do
-        feature.value
-      end
-    end
-  end
-end
