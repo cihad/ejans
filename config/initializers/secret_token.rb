f5c95f21c1d1eebdd288db8fb5937e1b182710f7
Cihad Paksoy <cihadp@gmail.com>
2012-09-20 18:39:39 +0300

    fixing: added default sets test and production env

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Ejans::Application.config.secret_token = APP_CONFIG['secret_token']
+binding.pry
