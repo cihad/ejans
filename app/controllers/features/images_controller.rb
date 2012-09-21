0e61310a38b47ffadde2f38cd355d1e8c89e4f30
Cihad Paksoy <cihadp@gmail.com>
2012-09-21 16:19:26 +0300

    image controller must be in features module on production

---
+++ app/controllers/features/images_controller.rb
@@ -0,0 +1,31 @@
+module Features
+  class ImagesController < ApplicationController
+    before_filter :node, :feature
+    respond_to :js
+    include ControllerHelper
+
+    def create
+      @images = @feature.add_images(params[:features_image][:image])
+    end
+
+    def destroye
+      @image = @feature.value.find(params[:id])
+      @image.destroy
+    end
+
+    def sort
+      sort_fields params[:features_image], @feature.value
+      render nothing: true
+    end
+
+    private
+    def node
+      @node = Node.find(params[:node_id])
+      @node.build_assoc!
+    end
+
+    def feature
+      @feature = @node.features.find(params[:feature_id])
+    end
+  end
+end
