f4d4eb6f137565d86eb69e86616079ebed87c289
Cihad Paksoy <cihadp@gmail.com>
2012-09-27 14:57:01 +0300

    added: integer form placeholder and suffix value

--- app/presenters/form_fields/integer_form_field.rb
+++ app/presenters/form_fields/integer_form_field.rb
@@ -15,5 +15,19 @@ module FormFields
     def filter_type
       conf.filter_type
     end
+
+    def suffix
+      conf.suffix
+    end
+
+    def placeholder
+      if minimum and maximum
+        "#{minimum}-#{maximum} arasi bir deger giriniz."
+      elsif maximum
+        "#{maximum}'a esit veya daha kucuk bir deger giriniz."
+      elsif minimum and minimum != 0
+        "#{minimum}'a esit veya daha buyuk bir deger giriniz."
+      end          
+    end
   end
 end
