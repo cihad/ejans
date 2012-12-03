fe2b6ee8d580ecdce71a83dbd008894f12f76f64
Cihad Paksoy <cihadp@gmail.com>
2012-12-03 14:58:28 +0200

    not spesific ruby version

--- config/recipes/resque.rb
+++ config/recipes/resque.rb
@@ -1,10 +1,22 @@
-# This file uses "capistrano-resque" gem
-# https://github.com/sshingler/capistrano-resque
-require "capistrano-resque"
+# Gist: https://gist.github.com/797301
+after "deploy:create_symlink", "deploy:restart_workers"
 
-role :resque_worker, domain
-role :resque_scheduler, domain
+##
+# Rake helper task.
+# http://pastie.org/255489
+# http://geminstallthat.wordpress.com/2008/01/27/rake-tasks-through-capistrano/
+# http://ananelson.com/said/on/2007/12/30/remote-rake-tasks-with-capistrano/
+def run_remote_rake(rake_cmd)
+  rake_args = ENV['RAKE_ARGS'].to_s.split(',')
+  cmd = "cd #{fetch(:latest_release)} && #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
+  cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
+  run cmd
+  set :rakefile, nil if exists?(:rakefile)
+end
 
-set :workers, { "marketing_queue" => 1 }
-
-after "deploy:restart", "resque:restart"
+namespace :deploy do
+  desc "Restart Resque Workers"
+  task :restart_workers, :roles => :db do
+    run_remote_rake "resque:restart_workers"
+  end
+end
