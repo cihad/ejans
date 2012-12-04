3e78f91c57b1dc750947ae03103796e3b42e1ead
Cihad Paksoy <cihadp@gmail.com>
2012-12-04 12:52:12 +0200

    added: check git revision

---
+++ config/recipes/default.rb
@@ -0,0 +1,18 @@
+namespace :deploy do
+  desc "Config symlink"
+  task :app_config, :except => { :no_release => true }, :role => :app do
+    put File.read("config/app_config.yml"), "#{shared_path}/config/app_config.yml"
+    run "ln -nfs #{shared_path}/config/app_config.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:app_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes"
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
