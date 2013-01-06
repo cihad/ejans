20b7239fc19667ab15d24a30b4dccc5877c6f056
Cihad Paksoy <cihadp@gmail.com>
2013-01-06 15:47:03 +0200

    devise added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: 'info@ejans.com'
 
-  def first_signin(user)
-    @user = user
-    mail(to: user.email, subject: 'Hesap bilgileri - Ejans.com')
-  end
-
   def notify_new_user_to_admin(user)
     @user = user
     mail(to: "cihad@ejans.com", subject: "Yeni uye - Ejans.com")
