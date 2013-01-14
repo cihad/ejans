4cd88e2cac07784f96b12efb228dae10e25c294a
Cihad Paksoy <cihadp@gmail.com>
2013-01-14 18:07:05 +0200

    unicorn restart

--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -16,11 +16,19 @@ namespace :unicorn do
   end
   after "deploy:setup", "unicorn:setup"
 
-  %w[start stop restart].each do |command|
+  %w[start stop].each do |command|
     desc "#{command} unicorn"
     task command, roles: :app do
       run "#{sudo} service unicorn_#{application} #{command}"
     end
     after "deploy:#{command}", "unicorn:#{command}"
   end
+
+  desc "restart unicorn" 
+  task :restart, roles: :app do
+    stop
+    start
+  end
+  after "deploy:restart", "unicorn:restart"
+
 end
